5f6e56f785ad42a13dca51a59cf6b75240660d8b
Tarun <tarunrox21@gmail.com>
2023-02-19 21:50:07 +0530

    commit 2 fixed

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Changes made"
+  cout << "Changes made";
   return 0;
 }
